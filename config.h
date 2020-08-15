e8a115b39f4450dbae9961052cd61a4c38c917f3
Toni Uhlig <matzeton@googlemail.com>
2020-08-15 10:48:38 +0200

    adjust some config values
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 #define DISTRIBUTOR_HOST "127.0.0.1"
 #define DISTRIBUTOR_PORT 7000
 
-#define NETWORK_BUFFER_MAX_SIZE 8192
+#define NETWORK_BUFFER_MAX_SIZE 8448 /* 8192 + 256 */
 
 /* nDPId default config options */
 #define nDPId_PIDFILE "/tmp/ndpid.pid"
@@ -15,7 +15,7 @@
 #define nDPId_TICK_RESOLUTION 1000
 #define nDPId_MAX_READER_THREADS 4
 #define nDPId_IDLE_SCAN_PERIOD 10000 /* 10 sec */
-#define nDPId_MAX_IDLE_TIME 7200000 /* 7200 sec */
+#define nDPId_MAX_IDLE_TIME 600000 /* 600 sec */
 #define nDPId_MAX_POST_END_FLOW_TIME 60000 /* 60 sec */
 #define nDPId_INITIAL_THREAD_HASH 0x03dd018b
 #define nDPId_MAX_PACKETS_PER_FLOW_TO_SEND 15
