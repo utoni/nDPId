b6b62ac883b32dbe2d31a0b13c32a7752182f0d2
Toni Uhlig <matzeton@googlemail.com>
2020-09-26 00:51:59 +0200

    Use --squash for git subtree pull.
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- contrib/update_jsmn.sh
+++ contrib/update_jsmn.sh
@@ -3,4 +3,4 @@
 MYDIR="$(dirname ${0})"
 cd "${MYDIR}/.."
 
-git subtree pull --prefix=contrib/jsmn https://github.com/zserge/jsmn.git master
+git subtree pull --squash --prefix=contrib/jsmn https://github.com/zserge/jsmn.git master
