9a861b0a4f84da8f14fe0d3b5fc7d461d64ec776
Toni Uhlig <matzeton@googlemail.com>
2026-06-08 13:11:30 +0200

    Removed Copilot leftover
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- build-validate/CMakeFiles/clean-libnDPI.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean-libnDPI"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean-libnDPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
